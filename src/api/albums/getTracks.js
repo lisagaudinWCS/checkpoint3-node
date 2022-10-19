04d4e38a3f8277eda40b2ab0b409fed3cc02d88e
lisagaudinWCS <lisagaudin.wcs@gmail.com>
2022-10-19 14:19:30 +0200

    end of checkpoint

--- src/api/albums/getTracks.js
+++ src/api/albums/getTracks.js
@@ -1,3 +1,24 @@
+const database = require('../database');
+
+// router.get('/:id/tracks', getTracks);
+
 module.exports = (req, res) => {
-  // your code here !
+  const id = parseInt(req.params.id);
+
+  database
+    .query(
+      'select * from album inner join track on album.id=track.id_album where album.id = ?',
+      [id]
+    )
+    .then(([albums]) => {
+      if (albums[0] != null) {
+        res.json(albums[0]);
+      } else {
+        res.status(404).send('Not Found');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving data from database');
+    });
 };
